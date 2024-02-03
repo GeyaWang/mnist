efa890decaf2a7432b542d5ba0806e7bb7ad808e
GeyaWang <geyaewang@gmail.com>
2024-02-03 15:01:34 +0000

    Accuracy calculation

--- load.py
+++ load.py
@@ -5,10 +5,31 @@ import mnist
 import random
 import numpy as np
 
+SAMPLE_SIZE = 10000
+
 
 def main():
-    model = Sequential.load('mnist_training.ptd')
+    model = Sequential.load('training.ptd')
     model.summary()
+
+    _, _, x, y = mnist.mnist('MNIST')
+
+    # shuffle data
+    zipped_data = list(zip(x, y))
+    random.shuffle(zipped_data)
+    x, y = zip(*zipped_data)
+    x = np.array(x)[:SAMPLE_SIZE]
+    y = np.array(y)[:SAMPLE_SIZE]
+
+    x_test = np.reshape(x, (*x.shape, 1))
+
+    y_pred = model.predict(x_test)
+    y_pred_argmax = np.argmax(y_pred, axis=1)
+
+    n_correct = np.sum(y_pred_argmax == y)
+
+    print()
+    print(f'accuracy: {n_correct}/{SAMPLE_SIZE} ({n_correct / SAMPLE_SIZE * 100}%)')
 
 
 if __name__ == '__main__':
